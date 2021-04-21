21a1df7511ea5ef7817a269365d2ceef647e2e34
Giovanni <indirizzoemail@mail.com>
2021-04-21 17:45:41 +0200

    make st compile on gentoo linux

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Fira Mono:pixelsize=20:antialias=true:autohint=true";
-static char *font2[] = { };
+static char *font2[] = { "" };
 static int borderpx = 2;
 
 /*

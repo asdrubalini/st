a04dd93f17d145646947121443db894f6a01c3f0
Asdrubalini <indirizzoemail@mail.com>
2021-10-10 17:21:15 +0200

    new font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Mono:pixelsize=20:antialias=true:autohint=true";
+static char *font = "Code New Roman:pixelsize=20:antialias=true:autohint=true";
 static char *font2[] = { "" };
 static int borderpx = 2;
 

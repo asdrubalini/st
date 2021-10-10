8598f4de315df641e5b016f774553f0485f2d492
Asdrubalini <indirizzoemail@mail.com>
2021-10-10 17:19:38 +0200

    fix merge conflict

--- config.h
+++ config.h
@@ -107,13 +107,9 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-<<<<<<< HEAD
-float alpha = 1;
-=======
 float alpha = 0.8;
 float alphaOffset = 0.0;
 float alphaUnfocus;
->>>>>>> e053bd6036331cc7d14f155614aebc20f5371d3a
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
